d5efb357c97216afc30d671d87fdd829b87acd1c
N Bhanu Priya <167521790+N-Bhanu-Priya18@users.noreply.github.com>
2026-09-10 13:15:21 +0530

    Create Day4_assessment.java

---
+++ Day4_assessment.java
@@ -0,0 +1,113 @@
+/*
+  Testcase1:
+*/
+
+CODE:
+package Problems;
+import java.time.Duration;
+import java.util.List;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Testcase3 
+{
+	public static void main(String[] args) throws InterruptedException 
+	{
+		//Launch the browser
+		WebDriver driver=new ChromeDriver();
+		
+		//Maximize the browser window
+		driver.manage().window().maximize();
+		
+		//Apply implicit wait
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		
+		//Navigate to the application
+		driver.get("https://www.instagram.com");
+		Thread.sleep(2000);
+		
+		//count and find the sizes of all the textfield		
+		List<WebElement> tf=driver.findElements(By.xpath("//input[@dir='ltr']"));
+		System.out.println(tf.size());//count:Gives number of elements in a list
+		for(WebElement ele:tf) 
+		{
+			System.out.println(ele.getSize());
+			if(ele.getDomAttribute("name").equals("email")) 
+			{
+				ele.sendKeys("bhanupriyan91@gmail.com");
+			}		
+		}		
+		Thread.sleep(2000);
+		
+		//close the browser
+		driver.quit();	
+	}
+
+}
+
+/*
+  Testcase2:
+*/
+CODE:
+package Problems;
+
+import java.time.Duration;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Testcase4 
+{
+	public static void main(String[] args) throws InterruptedException 
+	{
+		//Launch the browser
+		WebDriver driver=new ChromeDriver();
+		
+		//Maximize the browser window
+		driver.manage().window().maximize();
+		
+		//apply implicit wait
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		
+		//Navigate to the zomato application
+		driver.get("https://www.zomato.com/bangalore/delivery");
+		Thread.sleep(2000);
+		
+		//click on log in
+		WebElement login=driver.findElement(By.xpath("//button[text()='Log in']"));
+		login.click();
+		
+		//Locate the whole frame
+		WebElement frame=driver.findElement(By.xpath("//iframe[@id='auth-login-ui']"));
+		
+		//switchTo()
+		driver.switchTo().frame(frame);
+		
+		//Locate phone number textfield
+		WebElement phonetf=driver.findElement(By.xpath("//input[@placeholder='Phone']"));
+		phonetf.sendKeys("9108456311");
+		Thread.sleep(1000);
+		
+		//use this click only if you want to see the main page
+//		//click on cross symbol
+//		driver.findElement(By.xpath("//i[@class='sc-rbbb40-1 cLEXmC sc-re4bd0-1 bKymEa']")).click();
+//		Thread.sleep(2000);
+		
+		//switch back to main page
+		driver.switchTo().defaultContent();
+		Thread.sleep(1000);
+		
+		//verify whether it has came back to main page or not
+		WebElement ref=driver.findElement(By.xpath("//div[text()='Delivery']"));
+		System.out.println(ref.getText());
+		
+		Thread.sleep(2000);
+		
+		//close the browser
+		driver.quit();
+	}
+}
+
